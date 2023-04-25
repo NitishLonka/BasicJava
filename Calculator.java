dd7a7e4697f2314dacf2967afd589e93ba578768
NitishLonka <nitishlonka13@gmail.com>
2023-04-25 13:31:33 -0400

    Assignment 2

--- Calculator.java
+++ Calculator.java
@@ -1,16 +1,24 @@
 
+import java.util.Arrays;
+
 public class Calculator {
 
 	//Declare two variables here
 	int number1=0;
 	int number2=0;
-	
+	int[] array1;
+	int[] array2;
 	Calculator(int number1,int number2)
 	{
        this.number1 = number1;
 	   this.number2 = number2;
 	}
 	
+	Calculator(int[] array1,int[] array2)
+	{
+		this.array1 = Arrays.copyOf(array1,array1.length);
+		this.array2 = Arrays.copyOf(array2,array2.length);
+	}
 	
 	//Pass two variables into the below method and return sum
 	public int sum(int number1,int number2) {
@@ -30,6 +38,26 @@ public class Calculator {
 	//Pass two variables into the below method and return difference
 	public  int subtract(int number1,int number2) {
 		return number1-number2;
+	}
+
+	public int[] measure(String type )
+	{
+	   //Both the arrays are equal in length...	
+		if(type!=""){
+
+		//Code here 
+       
+
+        //return an array of sums of corresponding numbers from array 1 and array 2 if type is "sum"
+		//return an array of products of corresponding numbers from array 1 and array 2  if type is "product"
+		//return an array of divisions of corresponding numbers from array 1 and array 2 if type is "division"
+		//return an array of sums of corresponding numbers from array 1 and array 2 if type is "subtract"
+
+        
+
+		}
+        return new int[3];		
+
 	}
 
 }
--- Testcases.java
+++ Testcases.java
@@ -1,22 +1,49 @@
 public class Testcases {
   public static void main(String[] args) {
   // TODO Auto-generated method stub
-      Calculator cl = new Calculator(6,3);
-    if(cl.sum(6,3)==9)
+    // Calculator cl = new Calculator(6,3);
+    // if(cl.sum(6,3)==9)
+    // {
+    //   System.out.println("Sum Executed successfully");
+    // }
+    // if(cl.multiply(6,3)==18)
+    // {
+    //   System.out.println("Multiply Executed successfully");
+    // }
+    // if(cl.division(6,3)==2)
+    // {
+    //   System.out.println("division Executed successfully");
+    // }
+    // if(cl.subtract(6,3)==3)
+    // {
+    //   System.out.println("Subtract Executed successfully");
+    // }
+
+    Calculator cl = new Calculator(new int[]{1,1,1},new int[]{1,1,1});
+    int[] result;
+    result = cl.measure("sum");
+    System.out.println("sum:");
+    for(int i=0;i<result.length;i++)
     {
-      System.out.println("Sum Executed successfully");
+      System.out.print(result[i]+",");
     }
-    if(cl.multiply(6,3)==18)
+    System.out.println();
+    result = cl.measure("product");
+    for(int i=0;i<result.length;i++)
     {
-      System.out.println("Multiply Executed successfully");
+      System.out.print(result[i]+",");
     }
-    if(cl.division(6,3)==2)
+    System.out.println();
+    result = cl.measure("division");
+    for(int i=0;i<result.length;i++)
     {
-      System.out.println("division Executed successfully");
+      System.out.print(result[i]+",");
     }
-    if(cl.subtract(6,3)==3)
+    System.out.println();
+    result = cl.measure("subtract");
+    for(int i=0;i<result.length;i++)
     {
-      System.out.println("Subtract Executed successfully");
+      System.out.print(result[i]+",");
     }
 }
 }
